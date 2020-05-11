b5f5c09f827b5ee80dbf0007aa7dc3189082dcbc
Tyler Cox <tycox94@gmail.com>
2020-05-11 17:49:37 -0400

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+// Core
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// External Components
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Components
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SessionComponent } from './session/session.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,6 +16,8 @@ import { BuckitListComponent } from './home/buckit-list/buckit-list.component';
 import { CategoryListComponent } from './home/category-list/category-list.component';
 import { CategoryComponent } from './home/category-list/category/category.component';
 import { BuckitComponent } from './home/buckit-list/buckit/buckit.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,8 +30,9 @@ import { BuckitComponent } from './home/buckit-list/buckit/buckit.component';
     CategoryListComponent,
     CategoryComponent,
     BuckitComponent,
+    NavbarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
