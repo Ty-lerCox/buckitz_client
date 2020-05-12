90357e5cbd30d74bc0011a516cc05a7404ee6f02
Tyler Cox <tycox94@gmail.com>
2020-05-11 20:11:14 -0400

    session box UI

--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -1,15 +1,18 @@
+// Core
 import { Component, OnInit } from '@angular/core';
+
+// External Components
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-session',
   templateUrl: './session.component.html',
-  styleUrls: ['./session.component.scss']
+  styleUrls: ['./session.component.scss'],
 })
 export class SessionComponent implements OnInit {
+  faPlus = faPlus;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
